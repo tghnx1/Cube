aba734f466abefd2bc99a0dcfc853f7631310e94
tghnx1 <kokorevm2012@gmail.com>
2025-04-12 01:06:03 +0200

    video resise

--- Scripts/playButton.js
+++ Scripts/playButton.js
@@ -4,15 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const player = new Vimeo.Player(iframe);
     const playButton = document.getElementById('play-button');
 
-    // Request fullscreen
-    player.requestFullscreen().catch(error => {
-        console.error('Error enabling fullscreen:', error);
-    });
     // Play video on button click
     playButton.addEventListener('click', () => {
         player.play().then(() => {
             player.setVolume(1);
-
         }).catch(error => {
             console.error('Error playing the video:', error);
         });
